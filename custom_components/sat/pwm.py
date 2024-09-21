da4a932755d19fa10a28fd586841a59bd39d57ae
Sergeant "D <67208412+sergeantd83@users.noreply.github.com>
2024-09-21 19:57:08 +0300

    Another try

--- custom_components/sat/pwm.py
+++ custom_components/sat/pwm.py
@@ -5,6 +5,7 @@ from typing import Optional, Tuple
 
 from .const import HEATER_STARTUP_TIMEFRAME
 from .heating_curve import HeatingCurve
+from .coordinator import SatDataUpdateCoordinator
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -109,8 +110,8 @@ class PWM:
         if not self._automatic_duty_cycle:
             return int(self._last_duty_cycle_percentage * self._max_cycle_time), int((1 - self._last_duty_cycle_percentage) * self._max_cycle_time)
 
-        if self._last_duty_cycle_percentage < MIN_DUTY_CYCLE_PERCENTAGE:
-            return 0, 1800
+        if self._last_duty_cycle_percentage < MIN_DUTY_CYCLE_PERCENTAGE self._coordinator.flame_active:
+            return 180, 1620
 
         if self._last_duty_cycle_percentage <= DUTY_CYCLE_20_PERCENT:
             on_time = ON_TIME_20_PERCENT
@@ -132,6 +133,9 @@ class PWM:
 
         if self._last_duty_cycle_percentage > MAX_DUTY_CYCLE_PERCENTAGE:
             return 1800, 0
+
+        else:
+            return 0, 1800
 
     @property
     def state(self) -> PWMState:
