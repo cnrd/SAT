418512f2809edb83058996214aee13f7ada8a06b
Sergeant "D <67208412+sergeantd83@users.noreply.github.com>
2024-09-21 19:10:22 +0300

    Revert

--- custom_components/sat/pwm.py
+++ custom_components/sat/pwm.py
@@ -5,7 +5,6 @@ from typing import Optional, Tuple
 
 from .const import HEATER_STARTUP_TIMEFRAME
 from .heating_curve import HeatingCurve
-from .coordinator import SatDataUpdateCoordinator
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -27,7 +26,7 @@ class PWMState(str, Enum):
 class PWM:
     """A class for implementing Pulse Width Modulation (PWM) control."""
 
-    def __init__(self, heating_curve: HeatingCurve, max_cycle_time: int, automatic_duty_cycle: bool, coordinator: SatDataUpdateCoordinator, force: bool = False):
+    def __init__(self, heating_curve: HeatingCurve, max_cycle_time: int, automatic_duty_cycle: bool, force: bool = False):
         """Initialize the PWM control."""
         self._alpha = 0.2
         self._force = force
@@ -37,7 +36,6 @@ class PWM:
         self._heating_curve = heating_curve
         self._max_cycle_time = max_cycle_time
         self._automatic_duty_cycle = automatic_duty_cycle
-        self._coordinator = coordinator 
 
         self.reset()
 
@@ -111,9 +109,8 @@ class PWM:
         if not self._automatic_duty_cycle:
             return int(self._last_duty_cycle_percentage * self._max_cycle_time), int((1 - self._last_duty_cycle_percentage) * self._max_cycle_time)
 
-        # Updated condition with flame_active check
-        if self._last_duty_cycle_percentage < MIN_DUTY_CYCLE_PERCENTAGE and self._coordinator.flame_active:
-            return 180, 1620
+        if self._last_duty_cycle_percentage < MIN_DUTY_CYCLE_PERCENTAGE:
+            return 0, 1800
 
         if self._last_duty_cycle_percentage <= DUTY_CYCLE_20_PERCENT:
             on_time = ON_TIME_20_PERCENT
@@ -136,9 +133,6 @@ class PWM:
         if self._last_duty_cycle_percentage > MAX_DUTY_CYCLE_PERCENTAGE:
             return 1800, 0
 
-        else:
-            return 0, 1800
-
     @property
     def state(self) -> PWMState:
         """Returns the current state of the PWM control."""
@@ -157,4 +151,3 @@ class PWM:
     @property
     def last_duty_cycle_percentage(self):
         return round(self._last_duty_cycle_percentage * 100, 2)
-
