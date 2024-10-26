9b8ad9291d5dd8c653cb22608b80675e6b68096c
Sergeant "D <67208412+sergeantd83@users.noreply.github.com>
2024-10-26 07:42:13 +0300

    Update aggression value again

--- custom_components/sat/pid.py
+++ custom_components/sat/pid.py
@@ -257,7 +257,7 @@ class PID:
             return 73 if self._heating_system == HEATING_SYSTEM_UNDERFLOOR else 99
 
         if self._version == 2:
-            return 76.7
+            return 70.8
 
         raise Exception("Invalid version")
 
